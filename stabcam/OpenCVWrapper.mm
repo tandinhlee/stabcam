e0193c6aab62ae844aaa8e044a567186f92ae974
Dinh Le <tandinhlee@gmail.com>
2019-01-20 20:51:00 -0600

    Init stablization for live camera
    Prepare project for stabilization live camera

---
+++ stabcam/OpenCVWrapper.mm
@@ -0,0 +1,13 @@
+//
+//  OpenCVWrapper.m
+//  stabcam
+//
+//  Created by Dinh Le on 1/17/19.
+//  Copyright © 2019 Dinh Le. All rights reserved.
+//
+
+#import "OpenCVWrapper.h"
+using namespace cv;
+@implementation OpenCVWrapper
+
+@end
--- stabcam/ViewController.swift
+++ stabcam/ViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
 }
 
